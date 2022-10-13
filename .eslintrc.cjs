882d310c33e4477686b7dc4cfba7979458948152
Anson L <ansonlam82@gmail.com>
2022-10-13 00:26:43 -0400

    updated eslint, prettier and vue-tsc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,19 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  root: true,
   extends: [
     'plugin:vue/vue3-essential',
+    'plugin:jsonc/recommended-with-json',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
+  },
+  plugins: ['sort-keys-fix', 'jsonc'],
+  root: true,
+  rules: {
+    'sort-keys-fix/sort-keys-fix': ['warn', 'asc', { natural: true }],
   },
 };
