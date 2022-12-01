74d8155e23916df44dcced2278e2216ba4e7f0dc
Anson L <ansonlam82@gmail.com>
2022-11-30 20:26:43 -0500

    switched package manager to yarn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,60 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   extends: [
-    'plugin:vue/vue3-recommended',
-    'plugin:jsonc/recommended-with-json',
     'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:jsonc/recommended-with-json',
+    'plugin:vue/vue3-recommended',
     '@vue/eslint-config-typescript',
-    'prettier',
+    '@vue/eslint-config-prettier',
   ],
+  overrides: [
+    // {
+    //   files: '*.vue',
+    //   rules: {
+    //     'import/default': 'off',
+    //     'import/namespace': 'off',
+    //     'import/no-named-as-default': 'off',
+    //     'import/no-named-as-default-member': 'off',
+    //   },
+    // },
+    {
+      files: ['src/**', 'index.js'],
+      rules: {
+        'sort-keys-fix/sort-keys-fix': ['warn', 'asc', { natural: true }],
+      },
+    },
+  ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
-  plugins: ['sort-keys-fix', 'jsonc'],
+  plugins: [
+    'jsonc',
+    'import',
+    'simple-import-sort',
+    // 'sort-destructure-keys',
+    'sort-keys-fix',
+  ],
   root: true,
   rules: {
-    'sort-keys-fix/sort-keys-fix': ['warn', 'asc', { natural: true }],
+    'import/order': [
+      'warn',
+      {
+        'groups': ['builtin', 'external', 'parent', 'sibling', 'type'],
+        'newlines-between': 'always',
+        'pathGroups': [
+          { group: 'parent', pattern: '@/**', position: 'before' },
+        ],
+        'pathGroupsExcludedImportTypes': ['type'],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'off',
+    // 'sort-destructure-keys/sort-destructure-keys': 'warn',
     'vue/v-on-event-hyphenation': [
       'warn',
       'always',
@@ -23,5 +64,21 @@ module.exports = {
         autofix: true,
       },
     ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/external-module-folders': ['.yarn', 'node_modules'],
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: [
+          __dirname + '/tsconfig.json',
+          __dirname + '/tsconfig.config.json',
+        ],
+      },
+      node: true,
+    },
   },
 };
